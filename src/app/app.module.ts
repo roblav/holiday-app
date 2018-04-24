711d02edc3de680b7bf626fe570914545c8dac7c
robert.lavender <robert.lavender@digital.hmrc.gov.uk>
2018-04-24 15:26:08 +0100

    Add holidays module. Add bootstrap.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { HolidaysComponent } from './holidays/holidays.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HolidaysComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/holidays/holidays.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HolidaysComponent } from './holidays.component';
+
+@NgModule({
+  imports: [
+    CommonModule
+  ],
+  declarations: [HolidaysComponent]
+})
+export class HolidaysModule { }
